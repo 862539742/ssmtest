263de37ae4f3f7cfdcf55f7305784e967c0d2c7b
yang <929730168@qq.com>
2016-09-05 22:32:48 +0800

    shiro认证

--- WebContent/WEB-INF/jsp/index.jsp
+++ WebContent/WEB-INF/jsp/index.jsp
@@ -7,6 +7,25 @@
 <title>学生信息管理系统</title>
 </head>
 <body>
-成功登录学生信息管理系统
+	成功登录学生信息管理系统
+	<button id="btnLoginout">退出登录</button>
+
+	<button id="22">fff</button>
 </body>
+<script src="js/jquery-3.1.0.min.js"></script>
+<script type="text/javascript">
+
+	$("#btnLoginout").click(function() {
+		$.ajax({
+			type:"post",
+			url:"loginOut.action",
+			dataType:"JSON",
+			success:function(data){
+				alert(data.message)
+			}
+				
+		})
+	});
+
+</script>
 </html>
--- src/main/java/controller/Usercontroller.java
+++ src/main/java/controller/Usercontroller.java
@@ -1,13 +1,19 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.aop.AuthenticatedAnnotationHandler;
 import org.apache.shiro.crypto.CipherService;
@@ -18,6 +24,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import pojo.Userinfo;
 
@@ -29,23 +36,50 @@ import service.IUserService;
 public class Usercontroller{
 	@Resource
 	private IUserService userService = null;
-	@RequestMapping("/user.action")
-	public void test(HttpServletResponse response,String id) throws IOException{
+	@ResponseBody
+	@RequestMapping(value="/user.action",method=RequestMethod.GET)
+	public Userinfo test(HttpServletResponse response,String id) throws IOException{
 		Userinfo userinfo=userService.getUserById(id);
 		if (userinfo==null) {
 			response.getWriter().append("{\"result\":\"false\"}");
+			return null;
 		}else {
-			response.getWriter().append(JSON.toJSONString(userinfo));
+//			response.getWriter().append(JSON.toJSONString(userinfo));
+			return userinfo;
 		}
 	}
+	//登陆
 	@RequestMapping(value="/loginCheck.action",method=RequestMethod.POST)
-	public void loginCheck(String username,String password){
+	public String loginCheck(String username,String password){
 		Subject subject=SecurityUtils.getSubject();
-		AuthenticationToken token=new UsernamePasswordToken(username,password);
+		UsernamePasswordToken token=new UsernamePasswordToken(username,password);
 		System.out.println("========================================");
         System.out.println("1、进入认证方法");
-		subject.login(token);
-		Userinfo userinfo=(Userinfo)subject.getPrincipal();
+        try {
+        	subject.login(token);
+        	return "index";
+		}catch(UnknownAccountException uae){  
+            System.out.println("对用户[" + username + "]进行登录验证..验证未通过,未知账户");  
+        }catch(IncorrectCredentialsException ice){  
+            System.out.println("对用户[" + username + "]进行登录验证..验证未通过,错误的凭证");  
+        }catch(LockedAccountException lae){  
+            System.out.println("对用户[" + username + "]进行登录验证..验证未通过,账户已锁定");  
+            return "123";
+        }catch(ExcessiveAttemptsException eae){  
+            System.out.println("对用户[" + username + "]进行登录验证..验证未通过,错误次数过多");
+        }catch(AuthenticationException ae){  
+            //通过处理Shiro的运行时AuthenticationException就可以控制用户登录失败或密码错误时的情景  
+            System.out.println("对用户[" + username + "]进行登录验证..验证未通过,堆栈轨迹如下");  
+            ae.printStackTrace();  
+        } catch (Exception e) {
+		}
+        if (subject.isAuthenticated()) {
+        	System.out.println("dd");
+		}else {
+        	token.clear();
+		}
+        return "123";
+//		Userinfo userinfo=(Userinfo)subject.getPrincipal();
 	}
 	@RequestMapping("/main.action")
 	public String index(){
@@ -55,5 +89,11 @@ public class Usercontroller{
 	public void userByUsername(HttpServletResponse response,String userName) throws IOException{
 		Userinfo userinfo=userService.getUserByUsername(userName);
 		response.getWriter().append(JSON.toJSONString(userinfo));
+	}
+	@RequestMapping("/loginOut.action")
+	public void loginOut(HttpServletResponse response) throws IOException{
+		 SecurityUtils.getSubject().logout();  
+		 response.setCharacterEncoding("UTF-8");
+		response.getWriter().append("{\"result\":\"true\",\"message\":\"退出成功\"}");
 	}
 }	
