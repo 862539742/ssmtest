3762797eb7cced8cfc095d6b449c5dc9d07ecd2b
yangbiao <929730168@qq.com>
2016-09-09 09:06:11 +0800

    修改

--- src/main/java/controller/Usercontroller.java
+++ src/main/java/controller/Usercontroller.java
@@ -1,39 +1,27 @@
 package controller;
 
 import java.io.IOException;
-import java.util.List;
-
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.ExcessiveAttemptsException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.aop.AuthenticatedAnnotationHandler;
-import org.apache.shiro.crypto.CipherService;
-import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import pojo.SysUser;
-
 import com.alibaba.fastjson.JSON;
-
 import service.SysUserService;
 
 @Controller
 public class Usercontroller{
+	
 	@Resource
 	private SysUserService sysUserService = null;
 	
